20364a6038c40e3749ebf730896d4acef9f8d6e9
Jack Bentley <jack@craftsrv.com>
2017-03-24 19:53:57 +0000

    Initial commit
    Adds readme
    Adds license
    Adds base WHMCS module file

---
+++ README.md
@@ -0,0 +1,10 @@
+GamePanel.io WHMCS Provisioning Module
+======================================
+
+This module can be used to automate the provisioning of services from WHMCS
+ to your [GamePanel.io](https://gamepanel.io) instance.
+
+## License
+
+This module is licensed under the MIT license. See the `LICENSE` file for
+ more info.
---
+++ src/gamepanelio.php
@@ -0,0 +1,526 @@
+<?php
+
+if (!defined("WHMCS")) {
+    die("This file cannot be accessed directly");
+}
+
+/**
+ * Define module related meta data.
+ *
+ * Values returned here are used to determine module related abilities and
+ * settings.
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_Meta_Data_Parameters
+ *
+ * @return array
+ */
+function gamepanelio_MetaData()
+{
+    return [
+        'DisplayName' => 'GamePanel.io',
+        'APIVersion' => '1.1',
+        'RequiresServer' => true,
+        'DefaultNonSSLPort' => '80',
+        'DefaultSSLPort' => '443',
+    ];
+}
+
+/**
+ * Define product configuration options.
+ *
+ * The values you return here define the configuration options that are
+ * presented to a user when configuring a product for use with the module. These
+ * values are then made available in all module function calls with the key name
+ * configoptionX - with X being the index number of the field from 1 to 24.
+ *
+ * You can specify up to 24 parameters, with field types:
+ * * text
+ * * password
+ * * yesno
+ * * dropdown
+ * * radio
+ * * textarea
+ *
+ * Examples of each and their possible configuration parameters are provided in
+ * this sample function.
+ *
+ * @return array
+ */
+function gamepanelio_ConfigOptions()
+{
+    return [
+        'gpio_plan' => [
+            'FriendlyName' => 'Plan ID',
+            'Type' => 'text',
+            'Size' => '20',
+        ],
+        'gpio_allocation' => [
+            'FriendlyName' => 'IP Allocation',
+            'Type' => 'dropdown',
+            'Options' => [
+                'auto' => 'Auto',
+                'dedicated' => 'Dedicated',
+            ],
+            'Auto,Dedicated',
+            'Default' => 'Auto',
+        ],
+        'gpio_usernamePrefix' => [
+            'FriendlyName' => 'Username Prefix',
+            'Type' => 'text',
+            'Size' => '25',
+        ],
+        'gpio_game' => [
+            'FriendlyName' => 'Game Type',
+            'Type' => 'dropdown',
+            'Options' => json_decode(file_get_contents('games.json')),
+        ],
+    ];
+}
+
+/**
+ * @param array $params
+ * @return array
+ */
+function gamepanelio_getModuleOptions($params)
+{
+    $allocation = $params["configoption2"];
+    if (array_key_exists('configoptions', $params) && array_key_exists('allocation', $params['configoptions'])) {
+        if (in_array(strtolower($params['configoptions']['allocation']), ['dedicated', 'auto'])) {
+            $allocation = $params['configoptions']['allocation'];
+        }
+    }
+
+    return [
+        'plan' => $params["configoption1"],
+        'allocation' => $allocation,
+        'usernamePrefix' => $params["configoption3"],
+        'game' => $params["configoption4"]
+    ];
+}
+
+/**
+ * Provision a new instance of a product/service.
+ *
+ * Attempt to provision a new instance of a given product/service. This is
+ * called any time provisioning is requested inside of WHMCS. Depending upon the
+ * configuration, this can be any of:
+ * * When a new order is placed
+ * * When an invoice for a new order is paid
+ * * Upon manual request by an admin user
+ *
+ * @param array $params common module parameters
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_SDK_Parameters
+ *
+ * @return string "success" or an error message
+ */
+function gamepanelio_CreateAccount(array $params)
+{
+    try {
+        // TODO: Create a server
+        
+    } catch (Exception $e) {
+        // Record the error in WHMCS's module log.
+        logModuleCall(
+            'gamepanelio',
+            __FUNCTION__,
+            $params,
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
+
+        return $e->getMessage();
+    }
+
+    return 'success';
+}
+
+/**
+ * Suspend an instance of a product/service.
+ *
+ * Called when a suspension is requested. This is invoked automatically by WHMCS
+ * when a product becomes overdue on payment or can be called manually by admin
+ * user.
+ *
+ * @param array $params common module parameters
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_SDK_Parameters
+ *
+ * @return string "success" or an error message
+ */
+function gamepanelio_SuspendAccount(array $params)
+{
+    try {
+        // TODO: Suspend a server
+        
+    } catch (Exception $e) {
+        // Record the error in WHMCS's module log.
+        logModuleCall(
+            'gamepanelio',
+            __FUNCTION__,
+            $params,
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
+
+        return $e->getMessage();
+    }
+
+    return 'success';
+}
+
+/**
+ * Un-suspend instance of a product/service.
+ *
+ * Called when an un-suspension is requested. This is invoked
+ * automatically upon payment of an overdue invoice for a product, or
+ * can be called manually by admin user.
+ *
+ * @param array $params common module parameters
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_SDK_Parameters
+ *
+ * @return string "success" or an error message
+ */
+function gamepanelio_UnsuspendAccount(array $params)
+{
+    try {
+        // TODO: Un-suspend a server
+
+    } catch (Exception $e) {
+        // Record the error in WHMCS's module log.
+        logModuleCall(
+            'gamepanelio',
+            __FUNCTION__,
+            $params,
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
+
+        return $e->getMessage();
+    }
+
+    return 'success';
+}
+
+/**
+ * Terminate instance of a product/service.
+ *
+ * Called when a termination is requested. This can be invoked automatically for
+ * overdue products if enabled, or requested manually by an admin user.
+ *
+ * @param array $params common module parameters
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_SDK_Parameters
+ *
+ * @return string "success" or an error message
+ */
+function gamepanelio_TerminateAccount(array $params)
+{
+    try {
+        // TODO: Terminate server
+
+    } catch (Exception $e) {
+        // Record the error in WHMCS's module log.
+        logModuleCall(
+            'gamepanelio',
+            __FUNCTION__,
+            $params,
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
+
+        return $e->getMessage();
+    }
+
+    return 'success';
+}
+
+/**
+ * Change the password for an instance of a product/service.
+ *
+ * Called when a password change is requested. This can occur either due to a
+ * client requesting it via the client area or an admin requesting it from the
+ * admin side.
+ *
+ * This option is only available to client end users when the product is in an
+ * active status.
+ *
+ * @param array $params common module parameters
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_SDK_Parameters
+ *
+ * @return string "success" or an error message
+ */
+function gamepanelio_ChangePassword(array $params)
+{
+    try {
+        // TODO: Change user password
+
+    } catch (Exception $e) {
+        // Record the error in WHMCS's module log.
+        logModuleCall(
+            'gamepanelio',
+            __FUNCTION__,
+            $params,
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
+
+        return $e->getMessage();
+    }
+
+    return 'success';
+}
+
+/**
+ * Upgrade or downgrade an instance of a product/service.
+ *
+ * Called to apply any change in product assignment or parameters. It
+ * is called to provision upgrade or downgrade orders, as well as being
+ * able to be invoked manually by an admin user.
+ *
+ * This same function is called for upgrades and downgrades of both
+ * products and configurable options.
+ *
+ * @param array $params common module parameters
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_SDK_Parameters
+ *
+ * @return string "success" or an error message
+ */
+function gamepanelio_ChangePackage(array $params)
+{
+    try {
+        // TODO: Change server plan
+
+    } catch (Exception $e) {
+        // Record the error in WHMCS's module log.
+        logModuleCall(
+            'gamepanelio',
+            __FUNCTION__,
+            $params,
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
+
+        return $e->getMessage();
+    }
+
+    return 'success';
+}
+
+/**
+ * Test connection with the given server parameters.
+ *
+ * Allows an admin user to verify that an API connection can be
+ * successfully made with the given configuration parameters for a
+ * server.
+ *
+ * When defined in a module, a Test Connection button will appear
+ * alongside the Server Type dropdown when adding or editing an
+ * existing server.
+ *
+ * @param array $params common module parameters
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_SDK_Parameters
+ *
+ * @return array
+ */
+function gamepanelio_TestConnection(array $params)
+{
+    try {
+        // TODO: Add connection test
+        // Call the service's connection test function.
+
+        $success = true;
+        $errorMsg = '';
+    } catch (Exception $e) {
+        // Record the error in WHMCS's module log.
+        logModuleCall(
+            'gamepanelio',
+            __FUNCTION__,
+            $params,
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
+
+        $success = false;
+        $errorMsg = $e->getMessage();
+    }
+
+    return [
+        'success' => $success,
+        'error' => $errorMsg,
+    ];
+}
+
+/**
+ * Admin services tab additional fields.
+ *
+ * Define additional rows and fields to be displayed in the admin area service
+ * information and management page within the clients profile.
+ *
+ * Supports an unlimited number of additional field labels and content of any
+ * type to output.
+ *
+ * @param array $params common module parameters
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_SDK_Parameters
+ * @see gamepanelio_AdminServicesTabFieldsSave()
+ *
+ * @return array
+ */
+function gamepanelio_AdminServicesTabFields(array $params)
+{
+    try {
+        // Call the service's function, using the values provided by WHMCS in
+        // `$params`.
+        $response = array();
+
+        // TODO: Add ability to edit associated service
+        // Return an array based on the function's response.
+        return [
+            'Service ID' => "TODO"
+        ];
+    } catch (Exception $e) {
+        // Record the error in WHMCS's module log.
+        logModuleCall(
+            'gamepanelio',
+            __FUNCTION__,
+            $params,
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
+
+        // In an error condition, simply return no additional fields to display.
+    }
+
+    return array();
+}
+
+/**
+ * Execute actions upon save of an instance of a product/service.
+ *
+ * Use to perform any required actions upon the submission of the admin area
+ * product management form.
+ *
+ * It can also be used in conjunction with the AdminServicesTabFields function
+ * to handle values submitted in any custom fields which is demonstrated here.
+ *
+ * @param array $params common module parameters
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_SDK_Parameters
+ * @see gamepanelio_AdminServicesTabFields()
+ */
+function gamepanelio_AdminServicesTabFieldsSave(array $params)
+{
+    // TODO: Save any service changes
+    // Fetch form submission variables.
+    $originalFieldValue = isset($_REQUEST['gamepanelio_original_uniquefieldname'])
+        ? $_REQUEST['gamepanelio_original_uniquefieldname']
+        : '';
+
+    $newFieldValue = isset($_REQUEST['gamepanelio_uniquefieldname'])
+        ? $_REQUEST['gamepanelio_uniquefieldname']
+        : '';
+
+    // Look for a change in value to avoid making unnecessary service calls.
+    if ($originalFieldValue != $newFieldValue) {
+        try {
+            // Call the service's function, using the values provided by WHMCS
+            // in `$params`.
+        } catch (Exception $e) {
+            // Record the error in WHMCS's module log.
+            logModuleCall(
+                'gamepanelio',
+                __FUNCTION__,
+                $params,
+                $e->getMessage(),
+                $e->getTraceAsString()
+            );
+
+            // Otherwise, error conditions are not supported in this operation.
+        }
+    }
+}
+
+/**
+ * Client area output logic handling.
+ *
+ * This function is used to define module specific client area output. It should
+ * return an array consisting of a template file and optional additional
+ * template variables to make available to that template.
+ *
+ * The template file you return can be one of two types:
+ *
+ * * tabOverviewModuleOutputTemplate - The output of the template provided here
+ *   will be displayed as part of the default product/service client area
+ *   product overview page.
+ *
+ * * tabOverviewReplacementTemplate - Alternatively using this option allows you
+ *   to entirely take control of the product/service overview page within the
+ *   client area.
+ *
+ * Whichever option you choose, extra template variables are defined in the same
+ * way. This demonstrates the use of the full replacement.
+ *
+ * Please Note: Using tabOverviewReplacementTemplate means you should display
+ * the standard information such as pricing and billing details in your custom
+ * template or they will not be visible to the end user.
+ *
+ * @param array $params common module parameters
+ *
+ * @see http://docs.whmcs.com/Provisioning_Module_SDK_Parameters
+ *
+ * @return array
+ */
+function gamepanelio_ClientArea(array $params)
+{
+    // TODO: Make a front-end for the users
+    // Determine the requested action and set service call parameters based on
+    // the action.
+    $requestedAction = isset($_REQUEST['customAction']) ? $_REQUEST['customAction'] : '';
+
+    if ($requestedAction == 'manage') {
+        $serviceAction = 'get_usage';
+        $templateFile = 'templates/manage.tpl';
+    } else {
+        $serviceAction = 'get_stats';
+        $templateFile = 'templates/overview.tpl';
+    }
+
+    try {
+        // Call the service's function based on the request action, using the
+        // values provided by WHMCS in `$params`.
+        $response = array();
+
+        $extraVariable1 = 'abc';
+        $extraVariable2 = '123';
+
+        return array(
+            'tabOverviewReplacementTemplate' => $templateFile,
+            'templateVariables' => array(
+                'extraVariable1' => $extraVariable1,
+                'extraVariable2' => $extraVariable2,
+            ),
+        );
+    } catch (Exception $e) {
+        // Record the error in WHMCS's module log.
+        logModuleCall(
+            'gamepanelio',
+            __FUNCTION__,
+            $params,
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
+
+        // In an error condition, display an error page.
+        return array(
+            'tabOverviewReplacementTemplate' => 'error.tpl',
+            'templateVariables' => array(
+                'usefulErrorHelper' => $e->getMessage(),
+            ),
+        );
+    }
+}
