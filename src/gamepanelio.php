306f7b81352542e4667541bde84d46d0932b74ae
Jack Bentley <jack@craftsrv.com>
2017-03-29 15:47:38 +0100

    Adds client area output login button

--- src/gamepanelio.php
+++ src/gamepanelio.php
@@ -642,52 +642,10 @@ function gamepanelio_AdminServicesTabFieldsSave(array $params)
  */
 function gamepanelio_ClientArea(array $params)
 {
-    gamepanelio_checkUpdateDatabase();
-
-    // TODO: Make a front-end for the users
-    // Determine the requested action and set service call parameters based on
-    // the action.
-    $requestedAction = isset($_REQUEST['customAction']) ? $_REQUEST['customAction'] : '';
-
-    if ($requestedAction == 'manage') {
-        $serviceAction = 'get_usage';
-        $templateFile = 'templates/manage.tpl';
-    } else {
-        $serviceAction = 'get_stats';
-        $templateFile = 'templates/overview.tpl';
-    }
-
-    try {
-        // Call the service's function based on the request action, using the
-        // values provided by WHMCS in `$params`.
-        $response = array();
-
-        $extraVariable1 = 'abc';
-        $extraVariable2 = '123';
-
-        return array(
-            'tabOverviewReplacementTemplate' => $templateFile,
-            'templateVariables' => array(
-                'extraVariable1' => $extraVariable1,
-                'extraVariable2' => $extraVariable2,
-            ),
-        );
-    } catch (Exception $e) {
-        // Record the error in WHMCS's module log.
-        logModuleCall(
-            'gamepanelio',
-            __FUNCTION__,
-            $params,
-            $e->getMessage(),
-            $e->getTraceAsString()
-        );
-
-        // In an error condition, display an error page.
-        return array(
-            'tabOverviewReplacementTemplate' => 'error.tpl',
-            'templateVariables' => array(
-                'usefulErrorHelper' => $e->getMessage(),
-            ),
-        );
-    }
+    return [
+        'tabOverviewModuleOutputTemplate' => 'templates/overview.tpl',
+        'templateVariables' => [
+            'panelUrl' => ($params['serversecure'] ? 'https' : 'https') . '://' . $params['serverhostname'],
+        ],
+    ];
 }
---
+++ src/templates/overview.tpl
@@ -0,0 +1,1 @@
+<a href="{$panelUrl}" class="btn btn-primary btn-lg"><i class="fa fa-fw fa-key"></i> Login to control panel</a>
